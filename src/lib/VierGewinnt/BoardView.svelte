5986bc1f1053ae0314ba12a8604c71776821d138
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2024-01-05 01:27:17 +0100

    disabled buttons on win

--- src/lib/VierGewinnt/BoardView.svelte
+++ src/lib/VierGewinnt/BoardView.svelte
@@ -7,7 +7,7 @@
 <div class="flex flex-row gap-1 w-full bg-[#48547c] p-1 rounded-md">
     {#each board.columns as column, index}
       <div class="flex flex-col gap-1 flex-1">
-          <button on:click={() => {board.doMove(index); board = board}} class="aspect-square flex justify-center items-center">
+          <button on:click={() => {board.doMove(index); board = board}} disabled={board.getWinner() != Winner.Pending} class="aspect-square flex justify-center items-center">
             <svg class="w-7 h-7 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
             </svg>
